22754eaf7d5d1b83e248f1e8dc66bf3126463ae7
Sahar Avitan <58662825+saharavitan@users.noreply.github.com>
2020-10-30 23:13:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/saharavitan/webpt",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
