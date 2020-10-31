300a3d58b8323f0e9de24178106414b1258bde07
saharavitan <49635508+attackit@users.noreply.github.com>
2020-11-01 01:36:44 +0200

    Fixed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="webpt", 
-    version="1.2.4",
+    version="1.2.5",
     install_requires=['requests', 'bs4', 'urllib3'],
     packages=setuptools.find_packages(),
     include_package_data=True,
