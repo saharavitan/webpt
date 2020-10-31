c5f9a95ec6a5587bb1af390104ea0c7de2ebfc96
Sahar Avitan <58662825+saharavitan@users.noreply.github.com>
2020-11-01 01:10:18 +0200

    Version  1.2.4

---
+++ webpt/port_scanner.py
@@ -0,0 +1,45 @@
+import requests
+import socket
+import threading
+requests.packages.urllib3.disable_warnings() # noqa
+
+
+class ScanPort:
+    def __init__(self, target, start_port=None, end_port=None):
+        self.target = target
+        self.from_port = start_port
+        self.to_port = end_port
+        self.ports = []
+
+    def scanner(self, target, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.3)
+            sock.connect((target, port))
+            print(port)
+            self.ports.append(port)
+        except Exception as f: # noqa
+            pass
+
+    def __call__(self, *args, **kwargs):
+        if self.from_port is None:
+            self.from_port = 1
+        if self.to_port is None:
+            self.to_port = 10000
+        try:
+            num = 1
+            for port in range(self.from_port, self.to_port+1):
+                num += 1
+                t1 = threading.Thread(target=self.scanner, args=[self.target, port])
+                t1.start()
+                if num >= 50:
+                    for stop_thread in range(1, 100):
+                        t1.join()
+                    num = 0
+
+        except Exception as e: # noqa
+            pass
+
+
+def scanport(target, start_port=None, end_port=None):
+    return ScanPort(target, start_port, end_port)()
---
+++ webpt/response_analysis.py
@@ -0,0 +1,32 @@
+import requests
+import re
+requests.packages.urllib3.disable_warnings() # noqa
+
+
+class Find:
+    def __init__(self, src=None):
+        if src is not None:
+            self.src = src.replace("\n", "").replace("\r", "").replace("\t", "")
+        else:
+            self.src = ""
+        self.tag_list = None
+
+    def find(self, tag):
+        self.tag_list = re.findall(f"<{tag} .*?</{tag}>", self.src)
+        if not self.tag_list:
+            self.tag_list += re.findall(f"<{tag} .*?>", self.src)
+        return self.tag_list
+
+    def attrs(self, full_tag, attribute): # noqa
+        att_list = []
+        match = re.search(r'{}=[\'"]?([^\'" >]+)'.format(attribute), full_tag)
+        if match:
+            att_list.append(match.group(1))
+        if att_list:
+            return att_list
+        else:
+            return None
+
+
+def find(src=None):
+    return Find(src)
