7382c5b0f7a4934e1a40786df6f86b9a61ac4885
Sahar <Avitansahar@gmail.com>
2020-11-08 11:10:21 +0200

    Update License

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     url="https://github.com/saharavitan/webpt",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
