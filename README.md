c305e980ba11654a8f6e4c885c6e88328062835c
Sahar <Avitansahar@gmail.com>
2020-12-14 05:50:05 +0200

    Bug fixes and other minor improvements.

--- README.md
+++ README.md
@@ -232,7 +232,7 @@ Avitan
 * [SaharAvitan](https://twitter.com/avitansahar)
 
 ## Version
-**Current version is 2.3.0**
+**Current version is 2.3.1**
 
 ## What's new
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Webpt",
-    version="2.3.0",
+    version="2.3.1",
     install_requires=['requests', 'urllib3'],
     packages=setuptools.find_packages(),
     include_package_data=True,
